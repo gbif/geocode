bce6a3f09ab6d050837b9fcfcd9353f4a19ddd31
Matthew Blissett <matt@blissett.me.uk>
2020-08-17 12:45:01 +0200

    Add an arbitrary but consistent ordering, for locations exactly on the border.

--- database/all_layer_function.sql
+++ database/all_layer_function.sql
@@ -3,30 +3,30 @@ CREATE OR REPLACE FUNCTION query_layers(q_lng float, q_lat float, q_unc float, q
 RETURNS TABLE(layer text, id text, source text, title text, isoCountryCode2Digit character, distance float) AS $$
     (
       SELECT
-        'Political',
-        iso_a2,
-        'http://www.naturalearthdata.com/',
-        name,
-        iso_a2,
+        'Political' AS type,
+        iso_a2 AS id,
+        'http://www.naturalearthdata.com/' AS source,
+        name AS title,
+        iso_a2 AS isoCountryCode2Digit,
         ST_Distance(geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326)) AS distance
       FROM political
       WHERE ST_DWithin(geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326), q_unc)
         AND 'Political' = ANY(q_layers)
-      ORDER BY distance
+      ORDER BY distance, id
     )
   UNION ALL
     (
       SELECT DISTINCT
-        'EEZ' AS TYPE,
+        'EEZ' AS type,
         'http://marineregions.org/mrgid/' || eez.mrgid AS id,
-        'http://vliz.be/vmdcdata/marbound/' AS SOURCE,
+        'http://vliz.be/vmdcdata/marbound/' AS source,
         eez.geoname AS title,
         iso_map.iso2 AS isoCountryCode2Digit,
         ST_Distance(geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326)) AS distance
       FROM eez LEFT OUTER JOIN iso_map ON eez.iso_ter1 = iso_map.iso3
       WHERE ST_DWithin(eez.geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326), q_unc)
         AND 'EEZ' = ANY(q_layers)
-      ORDER BY distance
+      ORDER BY distance, id
     )
   UNION ALL
     (
@@ -40,9 +40,9 @@ RETURNS TABLE(layer text, id text, source text, title text, isoCountryCode2Digit
         WHERE ST_DWithin(gadm3.geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326), q_unc)
       )
       SELECT
-        'GADM0' AS TYPE,
+        'GADM0' AS type,
         gid_0 AS id,
-        'http://gadm.org/' AS SOURCE,
+        'http://gadm.org/' AS source,
         name_0 AS title,
         isoCountryCode2Digit,
         MIN(distance) AS distance
@@ -51,9 +51,9 @@ RETURNS TABLE(layer text, id text, source text, title text, isoCountryCode2Digit
       GROUP BY gid_0, name_0, isoCountryCode2Digit
       UNION ALL
       SELECT
-        'GADM1' AS TYPE,
+        'GADM1' AS type,
         gid_1 AS id,
-        'http://gadm.org/' AS SOURCE,
+        'http://gadm.org/' AS source,
         name_1 AS title,
         isoCountryCode2Digit,
         MIN(distance) AS distance
@@ -62,9 +62,9 @@ RETURNS TABLE(layer text, id text, source text, title text, isoCountryCode2Digit
       GROUP BY gid_1, name_1, isoCountryCode2Digit
       UNION ALL
       SELECT
-        'GADM2' AS TYPE,
+        'GADM2' AS type,
         gid_2 AS id,
-        'http://gadm.org/' AS SOURCE,
+        'http://gadm.org/' AS source,
         name_2 AS title,
         isoCountryCode2Digit,
         MIN(distance) AS distance
@@ -73,79 +73,79 @@ RETURNS TABLE(layer text, id text, source text, title text, isoCountryCode2Digit
       GROUP BY gid_2, name_2, isoCountryCode2Digit
       UNION ALL
       SELECT
-        'GADM3' AS TYPE,
+        'GADM3' AS type,
         gid_3 AS id,
-        'http://gadm.org/' AS SOURCE,
+        'http://gadm.org/' AS source,
         name_3 AS title,
         isoCountryCode2Digit,
         MIN(distance) AS distance
       FROM gadm
       WHERE 'GADM3' = ANY(q_layers) AND gid_3 IS NOT NULL
       GROUP BY gid_3, name_3, isoCountryCode2Digit
-      ORDER BY distance
+      ORDER BY distance, id
     )
   UNION ALL
     (
       SELECT
-        'IHO' AS TYPE,
+        'IHO' AS type,
         'http://marineregions.org/mrgid/' || mrgid AS id,
-        'http://marineregions.org/' AS SOURCE,
+        'http://marineregions.org/' AS source,
         name AS title,
         NULL,
         ST_Distance(geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326)) AS distance
       FROM iho
       WHERE ST_DWithin(geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326), q_unc)
         AND 'IHO' = ANY(q_layers)
-      ORDER BY distance
+      ORDER BY distance, id
     )
   UNION ALL
     (
       SELECT
-        'SeaVoX' AS TYPE,
+        'SeaVoX' AS type,
         skos_url AS id,
-        'http://marineregions.org/' AS SOURCE,
+        'http://marineregions.org/' AS source,
         sub_region AS title,
         NULL,
         ST_Distance(geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326)) AS distance
       FROM seavox
       WHERE ST_DWithin(geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326), q_unc)
         AND 'SeaVoX' = ANY(q_layers)
-      ORDER BY distance
+      ORDER BY distance, id
     )
   UNION ALL
     (
       SELECT
-        'WGSRPD' AS TYPE,
+        'WGSRPD' AS type,
         'WGSRPD:' || level4_cod AS id,
-        'http://www.tdwg.org/standards/109' AS SOURCE,
+        'http://www.tdwg.org/standards/109' AS source,
         level_4_na AS title,
         iso_code,
         ST_Distance(geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326)) AS distance
       FROM wgsrpd_level4
       WHERE ST_DWithin(geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326), q_unc)
         AND 'WGSRPD' = ANY(q_layers)
-      ORDER BY distance
+      ORDER BY distance, id
     )
   UNION ALL
     (
       SELECT
-        'GeolocateCentroids' AS TYPE,
+        'GeolocateCentroids' AS type,
         gid::text AS id,
-        'http://geo-locate.org/webservices/geolocatesvcv2/' AS SOURCE,
+        'http://geo-locate.org/webservices/geolocatesvcv2/' AS source,
         NULL,
         iso_a2 AS isoCountryCode2Digit,
         ST_Distance(geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326)) AS distance
       FROM geolocate_centroids
       WHERE ST_DWithin(geom, ST_SetSRID(ST_Point(q_lng, q_lat), 4326), q_unc)
         AND 'GeolocateCentroids' = ANY(q_layers)
-      ORDER BY distance
+      ORDER BY distance, id
     )
 $$ LANGUAGE SQL IMMUTABLE;
 
 SELECT
-  'SeaVoX' AS TYPE,
+  'SeaVoX' AS type,
   skos_url AS id,
-  'http://marineregions.org/' AS SOURCE,
+  'http://marineregions.org/' AS source,
   sub_region AS title,
   NULL,
   ST_Distance(geom, ST_SetSRID(ST_Point(4.02, 50.02), 4326))
