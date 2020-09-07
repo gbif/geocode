177bcb683042b6225d24f0ef2377e8827c0e35a5
fmendezh <federicomh@gmail.com>
2020-09-07 13:14:19 +0200

    Using the gadm_region view for all ws services, exposing elements stored as arrays split by | in the database as JSON arrays

--- geocode-api/src/main/java/org/gbif/geocode/api/model/GadmRegion.java
+++ geocode-api/src/main/java/org/gbif/geocode/api/model/GadmRegion.java
@@ -1,28 +1,76 @@
 package org.gbif.geocode.api.model;
 
+import java.util.List;
+
 public class GadmRegion {
 
-  private final String id;
+  private String id;
 
-  private final String name;
+  private String name;
 
-  private final String type;
+  private Integer gadmLevel;
 
-  public GadmRegion(String id, String name, String type) {
-    this.id = id;
-    this.name = name;
-    this.type = type;
-  }
+  private List<String> variantName;
+
+  private List<String> nonLatinName;
+
+  private List<String> type;
+
+  private List<String> englishType;
 
   public String getId() {
     return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
   }
 
   public String getName() {
     return name;
   }
 
-  public String getType() {
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getGadmLevel() {
+    return gadmLevel;
+  }
+
+  public void setGadmLevel(Integer gadmLevel) {
+    this.gadmLevel = gadmLevel;
+  }
+
+  public List<String> getVariantName() {
+    return variantName;
+  }
+
+  public void setVariantName(List<String> variantName) {
+    this.variantName = variantName;
+  }
+
+  public List<String> getNonLatinName() {
+    return nonLatinName;
+  }
+
+  public void setNonLatinName(List<String> nonLatinName) {
+    this.nonLatinName = nonLatinName;
+  }
+
+  public List<String> getType() {
     return type;
+  }
+
+  public void setType(List<String> type) {
+    this.type = type;
+  }
+
+  public List<String> getEnglishType() {
+    return englishType;
+  }
+
+  public void setEnglishType(List<String> englishType) {
+    this.englishType = englishType;
   }
 }
--- geocode-ws/src/main/java/org/gbif/geocode/ws/persistence/MyBatisConfiguration.java
+++ geocode-ws/src/main/java/org/gbif/geocode/ws/persistence/MyBatisConfiguration.java
@@ -2,6 +2,7 @@ package org.gbif.geocode.ws.persistence;
 
 import org.gbif.geocode.api.model.GadmRegion;
 import org.gbif.geocode.api.model.Location;
+import org.gbif.mybatis.type.StringArrayTypeHandler;
 
 import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ public class MyBatisConfiguration {
       configuration.setMapUnderscoreToCamelCase(true);
       configuration.getTypeAliasRegistry().registerAlias("Location", Location.class);
       configuration.getTypeAliasRegistry().registerAlias("GadmRegion", GadmRegion.class);
+      configuration.getTypeAliasRegistry().registerAlias("StringArrayTypeHandler", StringArrayTypeHandler.class);
     };
   }
 }
