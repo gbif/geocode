834bc7bad3e283736534e9a9922c9f891e1c76bd
ahakanzn <a.hakan.zn@gmail.com>
2024-05-28 11:26:40 +0200

    fixing tests

--- geocode-ws/src/test/java/org/gbif/geocode/ws/service/impl/ShapefilesVsPostgisComparisonIT.java
+++ geocode-ws/src/test/java/org/gbif/geocode/ws/service/impl/ShapefilesVsPostgisComparisonIT.java
@@ -188,6 +188,10 @@ public class ShapefilesVsPostgisComparisonIT {
 
   private void check(double latitude, double longitude, boolean debug) {
     System.out.println(i + "> Checking "+latitude+", "+longitude);
+    //TODO: Delete later
+    for (String pgLayer : pgLayers) {
+      System.out.println(pgLayer + " : " + sfLayers.size());
+    }
 
     boolean fail = false;
 
